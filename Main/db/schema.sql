5ce70fc81b55ad010f3354aebd8546ffc3a38d52
Hailey Flora <Haileycflora@gmail.com>
2023-04-15 10:33:54 -0400

    Add department table in schema.sql and seeds.sql

---
+++ Main/db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+)
---
+++ Main/db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO departments (id, department_name)
+VALUES 
+(001, "Admin" ),
+(002, "Beverage"), 
+(003, "Chefs"),
+(004, "Operations"),
+(005, "Sales");
