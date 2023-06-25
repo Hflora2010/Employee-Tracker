9ccdf544722fc115b67d9a3fb5bec87ddbfa86f0
Hailey Flora <Haileycflora@gmail.com>
2023-06-25 03:03:30 -0400

    update queries.js with viewAllDepartments(), viewAllEmployees(), viewAllRoles()

--- queries/queries.js
+++ queries/queries.js
@@ -4,7 +4,6 @@ async function viewAllDepartments() {
     try {
       const [rows] = await connection.query("SELECT * FROM department");
       console.table(rows);
-      // Call the startApp function again to prompt the user for the next action
       startApp();
     } catch (err) {
       console.error(err);
@@ -14,7 +13,7 @@ async function viewAllDepartments() {
 
 async function viewAllRoles() {
     try {
-        const [rows] = await connection.query("SELECT role.id, job_title, dpt_name AS department, salary FROM department INNER JOIN role ON department.id = role.department_id");
+        const [rows] = await connection.query("SELECT role.id, role.job_title, department.dpt_name AS department, role.salary FROM role JOIN department ON department.id = role.department_id");
         console.table(rows);
         startApp();
     } catch (err) {
@@ -22,16 +21,28 @@ async function viewAllRoles() {
     }
 }    
 
-  function viewAllEmployees() {
-    connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.job_title, department.dpt_name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id",
-      (err, results) => {
-        if (err) throw new err;
-        console.table(results);
-        // Call the startApp function again to prompt the user for the next action
-        startApp();
-      }
-    );
+async function viewAllEmployees() {
+    try {
+      const [rows] = await connection.query(`
+        SELECT 
+          employee.id,
+          employee.first_name,
+          employee.last_name,
+          role.job_title,
+          department.dpt_name,
+          role.salary,
+          CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        FROM 
+          employee
+          INNER JOIN role ON employee.role_id = role.id
+          INNER JOIN department ON role.department_id = department.id
+          LEFT JOIN employee manager ON employee.manager_id = manager.id
+      `);
+      console.table(rows);
+      startApp();
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-    module.exports = { viewAllDepartments, viewAllDepartments, viewAllRoles };
+    module.exports = { viewAllDepartments, viewAllEmployees, viewAllRoles };
